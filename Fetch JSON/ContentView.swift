055dd1d47cac439180e59a07627bd0747cfc0498
Caleb Wells <calebrwells@gmail.com>
2019-10-11 22:03:10 -0600

    Initial Commit

---
+++ Fetch JSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fetch JSON
+//
+//  Created by Caleb Wells on 10/11/19.
+//  Copyright © 2019 Caleb Wells. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
